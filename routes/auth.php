f559a205b4738375137f5726fe35e4532a6baf5b
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-06-19 22:19:08 -0400

    created the home page of the tenants with example data

--- routes/auth.php
+++ routes/auth.php
@@ -1,31 +1,7 @@
 <?php
-
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Resolvers\PathTenantResolver;
 
 Route::middleware('tenant')->prefix("{" . PathTenantResolver::$tenantParameterName . "}")->group(function () {
-    Route::middleware('guest')->group(function () {
 
-        Route::get('register', [RegisteredUserController::class, 'create'])
-            ->name('register');
-
-        Route::post('register', [RegisteredUserController::class, 'store']);
-
-        Route::get('login', [AuthenticatedSessionController::class, 'create'])
-            ->name('login');
-
-        Route::post('login', [AuthenticatedSessionController::class, 'store']);
-    });
-
-    Route::middleware('auth')->group(function () {
-
-        Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-
-        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-            ->name('logout');
-    });
 });
--- routes/tenant.php
+++ routes/tenant.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\ProfileController;
 use Stancl\Tenancy\Resolvers\PathTenantResolver;
 
 
@@ -24,23 +23,9 @@ use Stancl\Tenancy\Resolvers\PathTenantResolver;
 Route::middleware('tenant')->prefix("{" . PathTenantResolver::$tenantParameterName . "}")->group(function () {
 
     Route::get('/', function () {
-        return Inertia::render('Welcome', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-            // 'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-        ]);
+        return Inertia::render('Home', ['school', tenant()]);
     });
 
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
 });
 
 require __DIR__ . '/auth.php';
