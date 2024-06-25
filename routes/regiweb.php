05389a3dc0f73276c8ff53387c4102fbbf2992f9
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-06-24 22:41:46 -0400

    home page of regiweb

--- routes/regiweb.php
+++ routes/regiweb.php
@@ -8,10 +8,11 @@ Route::name('regiweb.')->prefix('regiweb')->group(function () {
     Route::middleware('guest:teacher')->group(function () {
         Route::get('login', [RegiwebLoginController::class, 'create'])->name('login.index');
         Route::post('login', [RegiwebLoginController::class, 'store'])->name('login');
-        Route::post('logout', [RegiwebLoginController::class, 'destroy'])->name('logout');
+
     });
 
     Route::middleware('auth:teacher')->group(function () {
+        Route::delete('logout', [RegiwebLoginController::class, 'destroy'])->name('logout');
         Route::get('/', [RegiwebHomeController::class, 'index'])->name('index');
         Route::get('/home', [RegiwebHomeController::class, 'home'])->name('home');
     });
