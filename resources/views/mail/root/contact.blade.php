a2797e2d8d74350eb5f91ee0e06ff0969be372a7
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-10-31 03:52:26 -0400

    Improve formatting in contact email template for better readability

--- resources/views/mail/root/contact.blade.php
+++ resources/views/mail/root/contact.blade.php
@@ -1,8 +1,12 @@
 <x-mail::message>
 # {{ __('mail/contact.title') }}
-## {{ __('mail/contact.name') }}: ## {{ $name }}
-## {{ __('mail/contact.email') }}: {{ $email }}
-## {{ __('mail/contact.message') }}: {{ $message }}
-## {{ __('mail/contact.phone') }}: {{ $phone }}
+
+**{{ __('mail/contact.name') }}:** {{ $name }}
+
+**{{ __('mail/contact.email') }}:**  {{ $email }}
+
+**{{ __('mail/contact.message') }}:**  {{ $message }}
+
+**{{ __('mail/contact.phone') }}:**  {{ $phone }}
 
 </x-mail::message>
