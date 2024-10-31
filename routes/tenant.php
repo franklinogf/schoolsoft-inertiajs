38ffa6bfb1d3d45c4854bc8a1b2f17b7a6c0a825
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-10-31 02:52:38 -0400

    Refactor route definitions for improved readability and consistency

--- routes/tenant.php
+++ routes/tenant.php
@@ -2,22 +2,21 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\FileUploadController;
 use App\Http\Controllers\Home\SchoolDocumentController;
 use App\Http\Controllers\Home\SchoolHomeController;
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Resolvers\PathTenantResolver;
 
-Route::middleware('tenant')->prefix('{' . PathTenantResolver::$tenantParameterName . '}')->group(function () {
+Route::middleware('tenant')->prefix('{'.PathTenantResolver::$tenantParameterName.'}')->group(function () {
     Route::name('home.')->group(function () {
         Route::get('/', SchoolHomeController::class)->name('index');
         Route::get('/documents', [SchoolDocumentController::class, 'index'])->name('documents');
         Route::get('/documents/download/{id}', [SchoolDocumentController::class, 'download'])->name('documents.download');
     });
 
-    require __DIR__ . '/admin.php';
-    require __DIR__ . '/regiweb.php';
-    require __DIR__ . '/parents.php';
-    require __DIR__ . '/foro.php';
+    require __DIR__.'/admin.php';
+    require __DIR__.'/regiweb.php';
+    require __DIR__.'/parents.php';
+    require __DIR__.'/foro.php';
 
 });
