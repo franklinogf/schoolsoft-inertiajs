40db49d3b56f9a05394ade4607bb9fa8099ec921
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-10-31 02:50:33 -0400

    Add English and Spanish translations for contact form fields

---
+++ lang/es/mail/contact.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'Formulario de contacto',
+    'name' => 'Nombre',
+    'email' => 'Correo electrónico',
+    'message' => 'Mensaje',
+    'phone' => 'Teléfono',
+];
