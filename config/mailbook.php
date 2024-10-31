d056062e0352580a7d8dfc0e4d64252ae6d80902
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-10-31 02:51:18 -0400

    Add configuration file for mailbook with various settings

---
+++ config/mailbook.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Enabled
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to enable or disable mailbook.
+    */
+    'enabled' => env('APP_ENV') === 'local',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database rollback
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to enable or disable database rollback.
+    | When enabled any changes to the database during the rendering
+    | of a mail will be rolled back.
+    */
+    'database_rollback' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Display preview
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to enable or disable the screen size preview button.
+    */
+    'display_preview' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Locales
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to define which languages you want
+    | to preview in mailbook.
+    */
+    'locales' => [
+        'en' => 'English',
+        'es' => 'Spanish',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Send
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to enable the send mail button.
+    */
+    'send' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Send to
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to specify where the send mail button should be
+    | sending to.
+    */
+    'send_to' => 'franklinomarflores@gmail.com',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Route prefix
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to define the route prefix that will be used on
+    | every route defined by mailbook.
+    */
+    'route_prefix' => '/mailbook',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Middlewares
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to define which middlewares will be used on
+    | every route defined by mailbook.
+    */
+    'middlewares' => [
+        Xammie\Mailbook\Http\Middlewares\RollbackDatabase::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Show credits
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to disable the text "Created with mailbook"
+    */
+    'show_credits' => false,
+];
