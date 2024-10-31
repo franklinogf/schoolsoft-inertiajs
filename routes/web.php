95e85e13e74cc67ca8bcf10f3f5b2de5bbe697f8
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-10-31 02:52:38 -0400

    Refactor route definitions for improved readability and consistency

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,11 @@ use App\Http\Controllers\Root\ContactController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', fn() => Inertia::render('Root/Index'))->name('home');
+Route::get('/', fn () => Inertia::render('Root/Index'))->name('home');
 
 Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
-Route::get('/modules', fn() => Inertia::render('Root/Modules'))->name('modules');
+Route::get('/modules', fn () => Inertia::render('Root/Modules'))->name('modules');
 
-Route::get('/regiweb', fn() => Inertia::render('Root/Regiweb'))->name('regiweb');
-
-
+Route::get('/regiweb', fn () => Inertia::render('Root/Regiweb'))->name('regiweb');
