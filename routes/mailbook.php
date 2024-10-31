7d7d28628b43a55a88d9f5e37998d27fe34a95d0
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-10-31 03:56:54 -0400

    Organize Mailbook routes into a category for better structure

--- routes/mailbook.php
+++ routes/mailbook.php
@@ -4,10 +4,12 @@ use App\Mail\MailbookMail;
 use App\Mail\Root\Contact;
 use Xammie\Mailbook\Facades\Mailbook;
 
-Mailbook::add(fn() => new Contact(
-    'Franklin Omar',
-    'Gonzalez Flores',
-    'franklinomarflores@gmail.com',
-    'Hola mundo!',
-    '+1 829-865-0000'
-));
+Mailbook::category('Primary page')->group(function () {
+    Mailbook::add(fn() => new Contact(
+        'Franklin Omar',
+        'Gonzalez Flores',
+        'franklinomarflores@gmail.com',
+        'Hola mundo!',
+        '+1 829-865-0000'
+    ));
+});
