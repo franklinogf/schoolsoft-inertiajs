6c0eac60f1390fb73b4f5f6e1205f710db945d2b
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-10-31 02:52:15 -0400

    Add mailbook routing for sending emails with contact information

---
+++ routes/mailbook.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Mail\MailbookMail;
+use App\Mail\Root\Contact;
+use Xammie\Mailbook\Facades\Mailbook;
+
+Mailbook::add(MailbookMail::class);
+Mailbook::add(fn () => new Contact(
+    'Franklin Omar',
+    'Gonzalez Flores',
+    'franklinomarflores@gmail.com',
+    'Hola mundo!',
+    '+1 829-865-0000'
+));
