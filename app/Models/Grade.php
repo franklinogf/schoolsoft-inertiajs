25fd2ee09515da3beaf7d1b8d8d0893d8a707ddb
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-01 17:08:31 -0400

    Add Grade model and establish relationship with Teacher model

---
+++ app/Models/Grade.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    public $timestamps = false;
+
+    protected $table = 'cursos';
+
+    protected $primaryKey = 'mt';
+
+    protected function descripcion(): Attribute
+    {
+        return Attribute::make(
+            get: fn(mixed $value, array $attributes) => app()->getLocale() === 'es'
+            ? $attributes['desc1']
+            : ($attributes['desc2'] === '' ? $attributes['desc1'] : $attributes['desc2']),
+        );
+    }
+
+}
