4ca0da1abd157cd9a0b6de6b5e71bd0b9243fb8d
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-01 17:08:49 -0400

    Refactor authentication types: rename Admin to AdminAuth and introduce TeacherAuth interface; add Teacher interface extending TeacherAuth with optional grades property

---
+++ resources/js/types/teacher.d.ts
@@ -0,0 +1,26 @@
+import { TeacherAuth } from "@/types/auth";
+
+interface Grade {
+  id: string;
+  curso: string;
+  year: string;
+  desc1: string;
+  desc2: string;
+  credito: number;
+  peso: number;
+  entrada: string;
+  salida: string;
+  dias: string;
+  maestro: string;
+  matri: number;
+  total: number;
+  ava: string;
+  valor: number;
+  orden: string;
+  verano: string;
+  mt: number;
+}
+
+export interface Teacher extends TeacherAuth {
+  grades?: Grade[];
+}
