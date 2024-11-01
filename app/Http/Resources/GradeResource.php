faef88dd8f053c8367a768a9efe56877e330724c
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-01 17:12:12 -0400

    Add GradeResource for transforming grade data into JSON format

---
+++ app/Http/Resources/GradeResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GradeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'curso' => $this->curso,
+            'year' => $this->year,
+            'desc1' => $this->desc1,
+            'desc2' => $this->desc2,
+            'credito' => $this->credito,
+            'peso' => $this->peso,
+            'entrada' => $this->entrada,
+            'salida' => $this->salida,
+            'dias' => $this->dias,
+            'maestro' => $this->maestro,
+            'matri' => $this->matri,
+            'total' => $this->total,
+            'ava' => $this->ava,
+            'valor' => $this->valor,
+            'orden' => $this->orden,
+            'verano' => $this->verano,
+            'mt' => $this->mt,
+        ];
+    }
+}
