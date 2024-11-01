9d206336c163a275c721daf0fe52e054838c1b60
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-01 15:33:38 -0400

    Add GenderEnum and YesNoEnum for improved type safety and clarity

---
+++ app/Enums/GenderEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum GenderEnum
+{
+    case MALE = 'm';
+
+    case FEMALE = 'f';
+}
---
+++ app/Enums/YesNoEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum YesNoEnum
+{
+    case YES = 'SI';
+
+    case NO = 'NO';
+}
