297453d8b94c98db742a47ecb5d26c9ffaab9840
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-05 00:54:17 -0500

    Add Active scope for filtering active students in queries

---
+++ app/Models/Scopes/Student/Active.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Scopes\Student;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class Active implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        $table = $model->getTable();
+        $builder->where("{$table}.fecha_baja", '0000-00-00');
+    }
+}
