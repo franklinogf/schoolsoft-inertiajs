86b30b606859b273261200667c2650d5857186a5
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-05 18:31:20 -0500

    Refactor i18next configuration to use modular translation files and improve missing key handling

--- resources/js/lib/i18next.ts
+++ resources/js/lib/i18next.ts
@@ -1,21 +1,31 @@
+import enCommon from "@/locales/en/common.json";
+import enHome from "@/locales/en/home.json";
+import esCommon from "@/locales/es/common.json";
+import esHome from "@/locales/es/home.json";
 import i18next from "i18next";
-import { initReactI18next } from "react-i18next";
-import enTranslations from "../../../lang/en.json";
-import esTranslations from "../../../lang/es.json";
-i18next.use(initReactI18next).init({
-  lng: "en",
-  fallbackLng: "es",
-  resources: {
-    en: {
-      translation: enTranslations,
-    },
-    es: {
-      translation: esTranslations,
-    },
-  },
+import { I18nextProviderProps, initReactI18next } from "react-i18next";
+i18next
+  .use(initReactI18next)
 
-  saveMissing: true,
-  missingKeyHandler(lngs, ns, key, fallbackValue, updateMissing, options) {
-    console.log("Missing key:", key, lngs);
-  },
-});
+  .init<I18nextProviderProps>({
+    ns: ["home", "common"],
+    defaultNS: "common",
+    lng: "en",
+    fallbackLng: "es",
+    debug: true,
+    saveMissing: true,
+    resources: {
+      en: {
+        home: enHome,
+        common: enCommon,
+      },
+      es: {
+        home: esHome,
+        common: esCommon,
+      },
+    },
+    missingKeyHandler(lngs, ns, key, fallbackValue, updateMissing, options) {
+      console.log("Missing key:", key);
+    },
+  });
+export default i18next;
