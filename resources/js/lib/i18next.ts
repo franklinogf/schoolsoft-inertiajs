dacde3b13cd191da1b438b83d7a3a4a6140ed698
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-05 17:25:36 -0500

    Add Spanish translations and update i18next configuration for multilingual support

--- resources/js/lib/i18next.ts
+++ resources/js/lib/i18next.ts
@@ -1,6 +1,7 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import enTranslations from "../../../lang/en.json";
+import esTranslations from "../../../lang/es.json";
 i18next.use(initReactI18next).init({
   lng: "en",
   fallbackLng: "es",
@@ -8,5 +9,13 @@ i18next.use(initReactI18next).init({
     en: {
       translation: enTranslations,
     },
+    es: {
+      translation: esTranslations,
+    },
+  },
+
+  saveMissing: true,
+  missingKeyHandler(lngs, ns, key, fallbackValue, updateMissing, options) {
+    console.log("Missing key:", key, lngs);
   },
 });
