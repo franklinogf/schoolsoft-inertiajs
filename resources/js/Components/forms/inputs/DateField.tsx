d98ef289abbf44ca49a6ff2c5e153c6ebd7fd2b6
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-04 23:44:00 -0500

    Enhance DateField component with clear button for resetting date input

--- resources/js/Components/forms/inputs/DateField.tsx
+++ resources/js/Components/forms/inputs/DateField.tsx
@@ -2,8 +2,10 @@ import { DateTimePicker, type DateTimePickerProps } from "@/Components/custom-ui
 import { FieldContainer } from "@/Components/forms/inputs/FieldContainer";
 import { FieldError } from "@/Components/forms/inputs/FieldError";
 import { FieldLabel } from "@/Components/forms/inputs/FieldLabel";
+import { Button } from "@/Components/ui/button";
 import { formatDateToString, formatStringToDate } from "@/lib/utils";
 import { enUS, es } from "date-fns/locale";
+import { XSquare } from "lucide-react";
 import { useId } from "react";
 import { useTranslation } from "react-i18next";
 interface DatePickerInputProps
@@ -40,20 +42,35 @@ export function DateField({
   return (
     <FieldContainer className={className}>
       <FieldLabel disabled={disabled} error={error} id={id} label={label} />
-      <DateTimePicker
-        disabled={disabled}
-        id={id}
-        yearRange={yearRange}
-        locale={language === "es" ? es : enUS}
-        displayFormat={displayFormat}
-        granularity="day"
-        placeholder={placeholder ?? t("Selecciona una fecha")}
-        value={formatStringToDate(data[name])}
-        onChange={(value) => {
-          setData(name, formatDateToString(value));
-        }}
-        {...props}
-      />
+      <div className="relative">
+        <DateTimePicker
+          disabled={disabled}
+          id={id}
+          yearRange={yearRange}
+          locale={language === "es" ? es : enUS}
+          displayFormat={displayFormat}
+          granularity="day"
+          placeholder={placeholder ?? t("Selecciona una fecha")}
+          value={formatStringToDate(data[name])}
+          onChange={(value) => {
+            setData(name, formatDateToString(value));
+          }}
+          {...props}
+        />
+        {data[name] && !disabled && (
+          <Button
+            onClick={() => {
+              setData(name, "");
+            }}
+            asChild
+            className="size-4"
+            size="icon"
+            variant="ghost"
+          >
+            <XSquare className="absolute right-2 top-1/2 -translate-y-1/2 hover:cursor-pointer" />
+          </Button>
+        )}
+      </div>
       <FieldError error={error} />
     </FieldContainer>
   );
