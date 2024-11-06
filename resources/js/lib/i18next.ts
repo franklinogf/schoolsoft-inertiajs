0cfce626accda442d006a13918dba60889ca27bb
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-05 19:24:56 -0500

    Refactor i18next configuration to use i18next-http-backend and remove hardcoded localization resources

--- resources/js/lib/i18next.ts
+++ resources/js/lib/i18next.ts
@@ -1,29 +1,15 @@
-import enCommon from "@/locales/en/common.json";
-import enHome from "@/locales/en/home.json";
-import esCommon from "@/locales/es/common.json";
-import esHome from "@/locales/es/home.json";
 import i18next from "i18next";
+import Backend from "i18next-http-backend";
 import { I18nextProviderProps, initReactI18next } from "react-i18next";
-i18next
+await i18next
   .use(initReactI18next)
-
+  .use(Backend)
   .init<I18nextProviderProps>({
-    ns: ["home", "common"],
+    ns: ["common", "home"],
     defaultNS: "common",
     lng: "en",
     fallbackLng: "es",
-    debug: true,
     saveMissing: true,
-    resources: {
-      en: {
-        home: enHome,
-        common: enCommon,
-      },
-      es: {
-        home: esHome,
-        common: esCommon,
-      },
-    },
     missingKeyHandler(lngs, ns, key, fallbackValue, updateMissing, options) {
       console.log("Missing key:", key);
     },
