b62a9dad57adb3d9fe640be8d494589bf1a51636
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-10 02:06:37 -0500

    Add Laravel Telescope integration with migration and schedule command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('telescope:prune --hours=48')->description('Delete entries from telescope tables daily')->daily();
