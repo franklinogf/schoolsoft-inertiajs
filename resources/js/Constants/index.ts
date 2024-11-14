d96ff9b232695d2d4f8c1695810679ecb975c1ff
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-14 01:08:26 -0500

    Refactor form select constants to use new enums and improve structure

--- resources/js/Constants/index.ts
+++ resources/js/Constants/index.ts
@@ -1,3 +1,5 @@
+import { GenderEnum, PagesEnum, TrimesterEnum, YesNoEnum } from "@/Enums";
+
 export const NO_PICTURE_BOY = "/assets/no-picture-boy.png";
 export const NO_PICTURE_GIRL = "/assets/no-picture-girl.png";
 export const NO_PICTURE_TEACHER = "/assets/no-picture-teacher.png";
@@ -10,3 +12,32 @@ export const LOGIN_REGIWEB_PICTURE = LOGO_REGIWEB;
 export const LOGIN_ADMIN_PICTURE = LOGO_SCHOOLSOFT;
 export const LOGIN_FORO_TEACHER_PICTURE = LOGO_SCHOOLSOFT;
 export const LOGIN_FORO_STUDENT_PICTURE = LOGO_SCHOOLSOFT;
+
+export const TRIMESTERS: Record<TrimesterEnum, string> = {
+  "Trimestre-1": "1er Trimestre",
+  "Trimestre-2": "2do Trimestre",
+  "Trimestre-3": "3er Trimestre",
+  "Trimestre-4": "4to Trimestre",
+  Verano: "Verano",
+};
+
+export const PAGES: Record<PagesEnum, string> = {
+  Notas: "Notas",
+  Notas2: "Notas2",
+  "Pruebas-Cortas": "Pruebas Cortas",
+  "Trab-Diarios": "Trabajos Diarios",
+  "Trab-Libreta": "Trabajos de Libreta",
+  "Cond-Asis": "Conducta y Asistencia",
+  "Ex-Final": "Examen Final",
+  "V-Nota": "Verano Notas",
+};
+
+export const YES_NO: Record<YesNoEnum, string> = {
+  SI: "Si",
+  NO: "No",
+};
+
+export const GENDERS: Record<GenderEnum, string> = {
+  m: "Masculino",
+  f: "Femenino",
+};
