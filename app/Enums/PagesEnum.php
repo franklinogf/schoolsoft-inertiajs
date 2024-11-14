d9bab1bf518dd4199036c0accd64682d1ebf9741
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-14 01:05:48 -0500

    Add GRADES2 case to PagesEnum for additional grade categorization

--- app/Enums/PagesEnum.php
+++ app/Enums/PagesEnum.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum PagesEnum: string
 {
     case GRADES = 'Notas';
+    case GRADES2 = 'Notas2';
     case SHORT_TESTS = 'Pruebas-Cortas';
     case DAILY_WORKS = 'Trab-Diarios';
     case NOTEBOOKS_WORKS = 'Trab-Libreta';
