42ba9b9e20e86fcc31cb10d0bba7a96f35b80ce8
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-16 00:46:53 -0500

    Refactor Notes pages to remove user prop from RegiwebLayout and simplify PageProps interface

--- resources/js/Pages/Regiweb/Notes/Show.tsx
+++ resources/js/Pages/Regiweb/Notes/Show.tsx
@@ -8,9 +8,6 @@ import { Switch } from "@/Components/ui/switch";
 import { PagesEnum, TrimesterEnum } from "@/Enums";
 import { RegiwebLayout } from "@/Layouts/Regiweb/RegiwebLayout";
 import { formatDate } from "@/lib/utils";
-import type { PagePropsWithUser } from "@/types";
-import type { Teacher } from "@/types/teacher";
-import { Head } from "@inertiajs/react";
 import { useId, useState } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -29,7 +26,7 @@ import {
   StudentsGrades,
 } from "./_types/studentsGrades";
 
-interface PageProps extends PagePropsWithUser<Teacher> {
+interface PageProps {
   course: string;
   page: PagesEnum;
   trimester: TrimesterEnum;
@@ -45,8 +42,7 @@ interface PageProps extends PagePropsWithUser<Teacher> {
   gradesValues: GradesValues | null;
   gradesValuesId: number | null;
 }
-export default function Show({
-  auth,
+export default function Page({
   course,
   page,
   trimester,
@@ -79,119 +75,112 @@ export default function Show({
     setValues(updatedValues);
   }
   return (
-    <>
-      <Head title={t("Entrada de notas")} />
-      <RegiwebLayout user={auth.user} title={t("Inicio")}>
-        <section className="space-y-4">
+    <RegiwebLayout title={t("Entrada de notas")}>
+      <section className="space-y-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>{t("Información")}</CardTitle>
+            <CardDescription hidden></CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
+              <InfoBadge label={t("Curso")} value={course} />
+              <InfoBadge label={t("Trimestre")} value={TRIMESTERS[trimester]} />
+              <InfoBadge label={t("Entrando notas a")} value={PAGES[page]} />
+              <InfoBadge label={t("Total de estudiantes")} value={studentsGrades.length} />
+              <InfoBadge
+                label={t("Fecha de inicio")}
+                value={formatDate(initialDate, { dateStyle: "long" })}
+              />
+              <InfoBadge
+                label={t("Fecha de cierre")}
+                value={formatDate(finalDate, { dateStyle: "long" })}
+              />
+              <InfoBadge label={t("Tipo de nota")} value={isPercent ? t("Porciento") : t("Suma")} />
+            </div>
+          </CardContent>
+        </Card>
+        {(page === PagesEnum.GRADES || page === PagesEnum.SUMMER_GRADES) && (
           <Card>
             <CardHeader>
-              <CardTitle>{t("Información")}</CardTitle>
+              <CardTitle>{t("Opciones")}</CardTitle>
               <CardDescription hidden></CardDescription>
             </CardHeader>
             <CardContent>
-              <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
-                <InfoBadge label={t("Curso")} value={course} />
-                <InfoBadge label={t("Trimestre")} value={TRIMESTERS[trimester]} />
-                <InfoBadge label={t("Entrando notas a")} value={PAGES[page]} />
-                <InfoBadge label={t("Total de estudiantes")} value={studentsGrades.length} />
-                <InfoBadge
-                  label={t("Fecha de inicio")}
-                  value={formatDate(initialDate, { dateStyle: "long" })}
+              <div className="space-y-4">
+                <SwitchCheckbox
+                  checked={isLetter}
+                  label={t("Convertir a letras")}
+                  description={
+                    page === PagesEnum.GRADES
+                      ? t("Esta opción es aplicada a la columna Nota 9 exclusivamente")
+                      : t("Esta opción es aplicada a la columna Nota 7 exclusivamente")
+                  }
                 />
-                <InfoBadge
-                  label={t("Fecha de cierre")}
-                  value={formatDate(finalDate, { dateStyle: "long" })}
+                <SwitchCheckbox
+                  checked={convert}
+                  onChange={setConvert}
+                  label={t("Conversión")}
+                  description={t("Esta opción convierte numeros a letras")}
                 />
-                <InfoBadge
-                  label={t("Tipo de nota")}
-                  value={isPercent ? t("Porciento") : t("Suma")}
-                />
+                {canEnd && (
+                  <SwitchCheckbox
+                    checked={hasEnded}
+                    label={t("Notificación")}
+                    description={t("Cuando el trimestre termine marque esta opción")}
+                  />
+                )}
               </div>
             </CardContent>
           </Card>
-          {(page === PagesEnum.GRADES || page === PagesEnum.SUMMER_GRADES) && (
-            <Card>
-              <CardHeader>
-                <CardTitle>{t("Opciones")}</CardTitle>
-                <CardDescription hidden></CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="space-y-4">
-                  <SwitchCheckbox
-                    checked={isLetter}
-                    label={t("Convertir a letras")}
-                    description={
-                      page === PagesEnum.GRADES
-                        ? t("Esta opción es aplicada a la columna Nota 9 exclusivamente")
-                        : t("Esta opción es aplicada a la columna Nota 7 exclusivamente")
-                    }
-                  />
-                  <SwitchCheckbox
-                    checked={convert}
-                    onChange={setConvert}
-                    label={t("Conversión")}
-                    description={t("Esta opción convierte numeros a letras")}
-                  />
-                  {canEnd && (
-                    <SwitchCheckbox
-                      checked={hasEnded}
-                      label={t("Notificación")}
-                      description={t("Cuando el trimestre termine marque esta opción")}
-                    />
-                  )}
-                </div>
-              </CardContent>
-            </Card>
-          )}
-          {page !== PagesEnum.GRADES &&
-          page !== PagesEnum.SUMMER_GRADES &&
-          page !== PagesEnum.FINAL_EXAM &&
-          page !== PagesEnum.CONDUCT_ATTENDANCE ? (
-            <Alert variant="warning">
-              <Info className="size-4" />
-              <AlertTitle>{t("Importante")}</AlertTitle>
-              <AlertDescription>
-                Recuerda ir a la pagina de notas y darle a grabar para tener los promédios
-                correctos.
-              </AlertDescription>
-            </Alert>
-          ) : null}
-        </section>
-        <section className="mt-8">
-          <OptionsContext.Provider value={{ page, trimester, course }}>
-            {values &&
-            amountOfGrades &&
-            (page === PagesEnum.GRADES ||
-              page === PagesEnum.SUMMER_GRADES ||
-              page === PagesEnum.SHORT_TESTS ||
-              page === PagesEnum.DAILY_WORKS ||
-              page === PagesEnum.NOTEBOOKS_WORKS) ? (
-              <DefaultForm
-                values={values}
-                amountOfGrades={amountOfGrades}
-                columns={columns}
-                convert={convert}
-                students={studentsGrades as StudentsDefaultGrades[]}
-              />
-            ) : page === PagesEnum.CONDUCT_ATTENDANCE ? (
-              <AttendaceForm students={studentsGrades as StudentsAttendanceGrades[]} />
-            ) : page === PagesEnum.FINAL_EXAM ? (
-              <ExamForm students={studentsGrades as StudentsExamGrades[]} />
-            ) : null}
-          </OptionsContext.Provider>
-        </section>
-        {values && gradesValuesId && amountOfGrades && (
-          <section className="mt-8">
-            <ValuesForm
-              values={values}
-              id={gradesValuesId}
-              amoutOfGrades={amountOfGrades}
-              onValueChange={handleValueChange}
-            />
-          </section>
         )}
-      </RegiwebLayout>
-    </>
+        {page !== PagesEnum.GRADES &&
+        page !== PagesEnum.SUMMER_GRADES &&
+        page !== PagesEnum.FINAL_EXAM &&
+        page !== PagesEnum.CONDUCT_ATTENDANCE ? (
+          <Alert variant="warning">
+            <Info className="size-4" />
+            <AlertTitle>{t("Importante")}</AlertTitle>
+            <AlertDescription>
+              Recuerda ir a la pagina de notas y darle a grabar para tener los promédios correctos.
+            </AlertDescription>
+          </Alert>
+        ) : null}
+      </section>
+      <section className="mt-8">
+        <OptionsContext.Provider value={{ page, trimester, course }}>
+          {values &&
+          amountOfGrades &&
+          (page === PagesEnum.GRADES ||
+            page === PagesEnum.SUMMER_GRADES ||
+            page === PagesEnum.SHORT_TESTS ||
+            page === PagesEnum.DAILY_WORKS ||
+            page === PagesEnum.NOTEBOOKS_WORKS) ? (
+            <DefaultForm
+              values={values}
+              amountOfGrades={amountOfGrades}
+              columns={columns}
+              convert={convert}
+              students={studentsGrades as StudentsDefaultGrades[]}
+            />
+          ) : page === PagesEnum.CONDUCT_ATTENDANCE ? (
+            <AttendaceForm students={studentsGrades as StudentsAttendanceGrades[]} />
+          ) : page === PagesEnum.FINAL_EXAM ? (
+            <ExamForm students={studentsGrades as StudentsExamGrades[]} />
+          ) : null}
+        </OptionsContext.Provider>
+      </section>
+      {values && gradesValuesId && amountOfGrades && (
+        <section className="mt-8">
+          <ValuesForm
+            values={values}
+            id={gradesValuesId}
+            amoutOfGrades={amountOfGrades}
+            onValueChange={handleValueChange}
+          />
+        </section>
+      )}
+    </RegiwebLayout>
   );
 }
 
