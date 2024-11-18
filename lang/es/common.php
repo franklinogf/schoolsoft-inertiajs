380e9384df0592a0e3f8ccafa2591767faa94311
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-18 00:31:33 -0500

    Add Spanish localization for common terms including grades, attendance, and exams

---
+++ lang/es/common.php
@@ -0,0 +1,41 @@
+<?php
+return [
+        
+        "bonus"=> [
+            "long"=>"Bono",
+            "short"=>"B"
+        ], 
+        "grades"=> [
+            "long"=>"Notas",
+            "short"=>"N"
+        ],
+        "grades2"=> [
+            "long"=>"Notas2",
+            "short"=>"N2"
+        ],
+        "shortTests"=> [
+            "long"=>"Pruebas cortas",
+            "short"=>"P-Cor"
+        ],
+        "dailyWork"=> [
+            "long"=>"Trabajos diarios",
+            "short"=>"T-Diarios"
+        ],
+        "notebookWork"=> [
+            "long"=>"Trabajos de libreta",
+            "short"=>"T-Libreta"
+        ],
+        "conductAttendance"=> [
+            "long"=>"Conducta y asistencia",
+            "short"=>"CA"
+        ],
+        "finalExam"=> [
+            "long"=>"Examen final",
+            "short"=>"EF"
+        ],
+        "summerGrades"=> [
+            "long"=>"Notas de verano",
+            "short"=>"NV"
+        ]
+    
+];
