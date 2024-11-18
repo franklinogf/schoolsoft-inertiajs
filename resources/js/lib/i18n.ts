f9c0e5ed1dd84e178d61784aa98df922aecf8d36
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-17 20:14:09 -0500

    Enhance i18n configuration by adding language detection and supporting multiple languages

--- resources/js/lib/i18n.ts
+++ resources/js/lib/i18n.ts
@@ -1,13 +1,29 @@
 import i18next from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 import { I18nextProviderProps, initReactI18next } from "react-i18next";
 await i18next
   .use(initReactI18next)
   .use(Backend)
+  .use(LanguageDetector)
   .init<I18nextProviderProps>({
-    ns: ["common", "home"],
+    detection: {
+      order: [
+        "htmlTag",
+        "querystring",
+        "cookie",
+        "localStorage",
+        "sessionStorage",
+        "navigator",
+        "path",
+        "subdomain",
+      ],
+    },
+    debug: false,
+    load: "languageOnly",
+    ns: ["input", "common", "home"],
     defaultNS: "common",
-    lng: "en",
+    supportedLngs: ["en", "es"],
     fallbackLng: "es",
     saveMissing: false,
     missingKeyHandler(lngs, ns, key, fallbackValue, updateMissing, options) {
