7746888577175e93a9de0940a2eadb49674570bb
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-17 20:14:13 -0500

    Add TypeScript definitions for i18next with custom namespaces and resources

---
+++ resources/js/types/i18next.d.ts
@@ -0,0 +1,20 @@
+import "i18next";
+import common from "../../../public/locales/es/common.json";
+import input from "../../../public/locales/es/input.json";
+import pages from "../../../public/locales/es/pages.json";
+
+declare module "i18next" {
+  // Extend CustomTypeOptions
+  interface CustomTypeOptions {
+    // custom namespace type, if you changed it
+    defaultNS: "common";
+    // custom resources type
+    resources: {
+      common: typeof common;
+      input: typeof input;
+      pages: typeof pages;
+      home: typeof input;
+    };
+    // other
+  }
+}
