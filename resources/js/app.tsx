b858d6d702f7eb6cfc5a96b293c4b3e94c85372d
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-19 21:42:00 -0500

    Remove app name from dynamic title in Inertia app configuration

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -8,10 +8,8 @@ import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createRoot } from "react-dom/client";
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
-
 createInertiaApp({
-  title: (title) => `${title} - ${appName}`,
+  title: (title) => `${title}`,
   resolve: async (name) => {
     const page = await resolvePageComponent(
       `./Pages/${name}.tsx`,
