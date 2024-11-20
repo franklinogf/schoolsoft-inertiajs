cbad33421cf2fe49fef0f832c048c74393e110bf
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-19 20:54:09 -0500

    Add English and Spanish localization files for grading and assessment terms

---
+++ lang/en/common.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'bonus' => [
+        'long' => 'Bonus',
+        'short' => 'B',
+    ],
+    'grades' => [
+        'long' => 'Notes',
+        'short' => 'G',
+    ],
+    'grades2' => [
+        'long' => 'Grades2',
+        'short' => 'G2',
+    ],
+    'shortTests' => [
+        'long' => 'Short tests',
+        'short' => 'S-Tests',
+    ],
+    'dailyWork' => [
+        'long' => 'Daily works',
+        'short' => 'D-Works',
+    ],
+    'notebookWork' => [
+        'long' => 'Notebook works',
+    ],
+];
