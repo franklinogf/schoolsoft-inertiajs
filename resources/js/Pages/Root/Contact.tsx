0f5eabffcf62413b97a14aa97955f8d560611c9b
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-19 21:26:41 -0500

    Enhance contact page localization with translation support for title and meta tags

--- resources/js/Pages/Root/Contact.tsx
+++ resources/js/Pages/Root/Contact.tsx
@@ -1,13 +1,15 @@
 import { ContactForm } from "@/Components/root/ContactForm";
 import { MaxWidthSection } from "@/Components/root/MaxWidthSection";
 import { Head } from "@inertiajs/react";
+import { useTranslation } from "react-i18next";
 
 export default function Page() {
+  const { t } = useTranslation("home", { keyPrefix: "contact" });
   return (
     <>
-      <Head title="Contacto" />
+      <Head title={t("meta.title")} />
       <MaxWidthSection>
-        <h1 className="title">Formulario de contacto</h1>
+        <h1 className="title">{t("title")}</h1>
         <ContactForm />
       </MaxWidthSection>
     </>
