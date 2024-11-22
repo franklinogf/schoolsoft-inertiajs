01c6a47268b8af290c5ec9937a200afd1d681ba5
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-22 17:30:13 -0500

    Refactor notes routes to improve structure and add AttendanceController for attendance management

--- routes/regiweb.php
+++ routes/regiweb.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Regiweb\Notes\AttendanceController;
 use App\Http\Controllers\Regiweb\Notes\RegiwebNotesController;
 use App\Http\Controllers\Regiweb\RegiwebHomeController;
 use App\Http\Controllers\Regiweb\RegiwebLoginController;
@@ -21,13 +22,26 @@ Route::name('regiweb.')->prefix('regiweb')->group(function () {
         Route::get('/profile', [RegiwebProfileController::class, 'show'])->name('profile.show');
         Route::post('/profile', [RegiwebProfileController::class, 'update'])->name('profile.update');
 
-        Route::name('notes.')->prefix('notes')->controller(RegiwebNotesController::class)->group(function () {
-            Route::get('/', 'index')->name('index');
-            Route::get('/enter-grades', 'show')->name('show');
-            Route::post('/enter-grades/default', 'saveDefault')->name('default.save');
-            Route::post('/enter-grades/attendance', 'saveAttendance')->name('attendance.save');
-            Route::post('/enter-grades/exam', 'saveExam')->name('exam.save');
-            Route::put('/enter-grades/values/{id}', 'saveValues')->name('values.save');
+        Route::name('notes.')->prefix('notes')->group(function () {
+            Route::controller(RegiwebNotesController::class)->group(function () {
+                Route::get('/', 'index')->name('index');
+                Route::prefix('enter-grades')->group(function () {
+                    Route::get('/', 'show')->name('show');
+                    Route::post('/default', 'saveDefault')->name('default.save');
+                    Route::post('/attendance', 'saveAttendance')->name('attendance.save');
+                    Route::post('/exam', 'saveExam')->name('exam.save');
+                    Route::put('/values/{id}', 'saveValues')->name('values.save');
+                });
+            });
+            Route::name('attendance.')->prefix('attendance')->controller(AttendanceController::class)->group(function () {
+                Route::get('/', 'entry')->name('entry');
+                Route::put('/update', 'update')->name('entry.update');
+                Route::get('/report', 'report')->name('report');
+                Route::get('/report/pdf', 'reportPdf')->name('report.pdf');
+                Route::get('/report/daily', 'dailyReport')->name('dailyReport');
+                Route::get('/report/daily/pdf', 'dailyReportPdf')->name('dailyReport.pdf');
+
+            });
         });
 
     });
