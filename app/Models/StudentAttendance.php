14dbbf3375206ad8fbc11ad21dd616d977cc641f
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-25 18:04:28 -0500

    Add scope to filter StudentAttendance records by date range

--- app/Models/StudentAttendance.php
+++ app/Models/StudentAttendance.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Scopes\SchoolYear;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 #[ScopedBy(SchoolYear::class)]
@@ -12,4 +13,9 @@ class StudentAttendance extends Model
     protected $table = 'asispp';
 
     public $timestamps = false;
+
+    public function scopeWhereDatesBetween(Builder $query, $date1, $date2)
+    {
+        return $query->whereDate('fecha', '>=', $date1)->whereDate('fecha', '<=', $date2);
+    }
 }
