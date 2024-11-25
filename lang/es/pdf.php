2726f7d574d3d4dcb4679bb8b12e3aadee97b06e
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-25 18:04:37 -0500

    Add Spanish language support for Attendance Report PDF

---
+++ lang/es/pdf.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'page' => 'Página',
+    'attendanceReport' => [
+        'metaTitle' => 'Informe de asistencia',
+        'schoolYear' => 'Año escolar',
+        'title' => 'Informe de estudiantes con problemas de tardanzas y ausentismo por horario de clase',
+        'teacher' => 'Maestro(a):',
+        'subject' => 'Asignatura:',
+        'grade' => 'Grado:',
+        'columns' => [
+            '1' => 'Nombre del estudiante',
+            '2' => 'Horario',
+            '3' => 'Ausencias',
+            '4' => 'Tardanzas',
+            '5' => 'Observaciones',
+        ],
+    ],
+];
