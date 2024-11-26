9ca7c4ba530f3904ca55761331eaaca7ba1b19ee
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-11-25 20:21:45 -0500

    Add student relationship to StudentAttendance model

--- app/Models/StudentAttendance.php
+++ app/Models/StudentAttendance.php
@@ -6,6 +6,7 @@ use App\Models\Scopes\SchoolYear;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 #[ScopedBy(SchoolYear::class)]
 class StudentAttendance extends Model
@@ -17,5 +18,10 @@ class StudentAttendance extends Model
     public function scopeWhereDatesBetween(Builder $query, $date1, $date2)
     {
         return $query->whereDate('fecha', '>=', $date1)->whereDate('fecha', '<=', $date2);
+    }
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class, 'mt', 'mt');
     }
 }
