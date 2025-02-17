b70e220776c9349fe33e829fda649e1d431b504f
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-02-16 20:11:10 -0500

    Enhance SubmitButton component with customizable className prop

--- resources/js/Components/forms/SubmitButton.tsx
+++ resources/js/Components/forms/SubmitButton.tsx
@@ -1,11 +1,22 @@
 import { Button, ButtonProps } from "@/Components/ui/button";
+import { cn } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 interface SubmitButtonProps extends ButtonProps {
   children: React.ReactNode;
 }
-export default function SubmitButton({ children, disabled, ...props }: SubmitButtonProps) {
+export default function SubmitButton({
+  children,
+  disabled,
+  className,
+  ...props
+}: SubmitButtonProps) {
   return (
-    <Button disabled={disabled} {...props} type="submit">
+    <Button
+      className={cn("cursor-pointer", className)}
+      disabled={disabled}
+      {...props}
+      type="submit"
+    >
       {disabled ? <Loader2 className="size-4 animate-spin" /> : children}
     </Button>
   );
