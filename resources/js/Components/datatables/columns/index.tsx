98be6de6f76d70a5b1430d256009dc90f18fc27b
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-02-17 12:59:38 -0500

    Enhance selection header in DataTable with labeled checkboxes for all and all page selection

--- resources/js/Components/datatables/columns/index.tsx
+++ resources/js/Components/datatables/columns/index.tsx
@@ -1,17 +1,35 @@
 import { Checkbox } from "@/Components/ui/checkbox";
+import { Label } from "@/Components/ui/label";
 import type { ColumnDef, RowData } from "@tanstack/react-table";
 
 export const selectionHeader: ColumnDef<RowData> = {
   id: "select",
   header: ({ table }) => (
-    <Checkbox
-      className="bg-background data-[state=checked]:bg-background data-[state=checked]:text-primary"
-      checked={
-        table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")
-      }
-      onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
-      aria-label="Select all"
-    />
+    <div className="flex flex-col space-y-2">
+      <Label className="flex items-center space-x-1">
+        <Checkbox
+          className="bg-background data-[state=checked]:bg-background data-[state=checked]:text-primary"
+          checked={
+            table.getIsAllRowsSelected() || (table.getIsSomeRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+        <span>All</span>
+      </Label>
+      <Label className="flex items-center space-x-1">
+        <Checkbox
+          className="bg-background data-[state=checked]:bg-background data-[state=checked]:text-primary"
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+        <span>All page</span>
+      </Label>
+    </div>
   ),
 
   cell: ({ row }) => (
