2a2bf396f054f3b48fe4dd5115d4322474d5f8be
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-06 02:01:35 -0500

    Update grid column classes in FieldsGrid component for improved responsiveness

--- resources/js/Components/forms/inputs/FieldsGrid.tsx
+++ resources/js/Components/forms/inputs/FieldsGrid.tsx
@@ -4,12 +4,12 @@ import { cva, type VariantProps } from "class-variance-authority";
 const inputsGridVariants = cva("grid grid-cols-1 gap-2", {
   variants: {
     cols: {
-      1: "md:grid-cols-1",
-      2: "md:grid-cols-2",
-      3: "md:grid-cols-3",
-      4: "md:grid-cols-4",
-      5: "md:grid-cols-5",
-      6: "md:grid-cols-6",
+      1: "lg:grid-cols-1",
+      2: "lg:grid-cols-2",
+      3: "lg:grid-cols-3",
+      4: "lg:grid-cols-4",
+      5: "lg:grid-cols-5",
+      6: "lg:grid-cols-6",
     },
   },
   defaultVariants: {
