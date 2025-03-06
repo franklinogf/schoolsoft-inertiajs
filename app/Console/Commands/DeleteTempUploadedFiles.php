cf6cfd827850b3b638f30a506b45335ffaf3e003
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-05 23:08:40 -0500

    Add command to delete temporary uploaded files older than 24 hours

---
+++ app/Console/Commands/DeleteTempUploadedFiles.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\TemporaryFile;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class DeleteTempUploadedFiles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-temp-uploaded-files';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete temporary uploaded files older than 24 hours.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Deleting temporary uploaded files older than 24 hours.');
+        TemporaryFile::whereDate('created_at', '<', now()->subDay())->get()->each(function (TemporaryFile $temporaryFile) {
+            $this->line("Deleting temporary file folder {$temporaryFile->folder}");
+            $temporaryFile->delete();
+        });
+        $this->info('Temporary uploaded files older than 24 hours have been deleted.');
+
+        $this->info('Deleting temporary uploaded files that still exist older than 24 hours.');
+        $storage = Storage::disk('local');
+        $directories = $storage->directories('tmp');
+        if (empty($directories)) {
+            $this->info('No temporary uploaded files older than 24 hours exist.');
+
+            return;
+        }
+        foreach ($directories as $directory) {
+            $directoryLastModified = Carbon::createFromTimestamp($storage->lastModified($directory));
+
+            if (now()->diffInHours($directoryLastModified) > 24) {
+                $storage->deleteDirectory($directory);
+            }
+        }
+        $this->info('Temporary uploaded files that still exist older than 24 hours have been deleted.');
+    }
+}
