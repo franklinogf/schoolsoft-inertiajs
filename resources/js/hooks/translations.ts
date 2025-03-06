eccdbe6712928d076cf1d4fe8930f7f92d11d64f
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-06 18:07:14 -0500

    Update translations to support English language in hooks

--- resources/js/hooks/translations.ts
+++ resources/js/hooks/translations.ts
@@ -1,11 +1,11 @@
 import { useLaravelReactI18n } from "laravel-react-i18n";
-import translations from "../../../lang/es.json";
-// import otherTranslations from "../../../lang/php_es.json";
+import translations from "../../../lang/en.json";
+// import otherTranslations from "../../../lang/php_en.json";
 
 type JsonTranslations = keyof typeof translations;
 // type OtherTranslations = keyof typeof otherTranslations;
 
-export type Translations = JsonTranslations;
+export type Translations = JsonTranslations & {};
 export function useTranslations() {
   const { t, setLocale, currentLocale, tChoice, loading, getLocales, isLocale } =
     useLaravelReactI18n<Translations>();
