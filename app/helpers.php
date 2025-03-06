8b3d0413ea1da1265dae5c004dbc077f789d2aff
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-06 17:30:05 -0500

    Add helper functions for temporary and public tenant paths

--- app/helpers.php
+++ app/helpers.php
@@ -21,3 +21,32 @@ if (! function_exists('get_extension')) {
         return '.'.pathinfo($file, PATHINFO_EXTENSION);
     }
 }
+
+if (! function_exists('tmp_path')) {
+    function tmp_path(?string $folder = null, ?string $filename = null)
+    {
+        $path = 'tmp';
+        if ($folder !== null) {
+            $folder = ltrim($folder, '/');
+            $path .= "/{$folder}";
+            if ($filename !== null) {
+                $filename = ltrim($filename, '/');
+                $path .= "/{$filename}";
+            }
+        }
+
+        return $path;
+    }
+}
+
+if (! function_exists('public_tenant_path')) {
+    function public_tenant_path(?string $path = null)
+    {
+        if (tenant() === null) {
+            abort(500, 'No tenant set');
+        }
+        $cleanPath = ltrim($path, '/');
+
+        return 'public/'.(tenant('id').'/'.$cleanPath);
+    }
+}
