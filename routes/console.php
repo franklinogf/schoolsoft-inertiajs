80261fd9b4296b453077c9fe03d4ed6ca1b0625f
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-05 23:14:21 -0500

    Add scheduled command to delete temporary uploaded files hourly

--- routes/console.php
+++ routes/console.php
@@ -1,3 +1,7 @@
 <?php
 
+use App\Console\Commands\DeleteTempUploadedFiles;
+
 Schedule::command('telescope:prune --hours=48')->description('Delete entries from telescope tables daily')->daily();
+
+Schedule::command(DeleteTempUploadedFiles::class)->hourly();
