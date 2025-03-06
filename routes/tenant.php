5dd4ee3413849f2137896fe7e8515bec3930cef8
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-06 17:30:44 -0500

    Refactor tenant route middleware to include InitializeTenancyByPath and SetDefaultTenant for improved tenant management

--- routes/tenant.php
+++ routes/tenant.php
@@ -4,19 +4,28 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Home\SchoolDocumentController;
 use App\Http\Controllers\Home\SchoolHomeController;
+use App\Http\Middleware\SetDefaultTenant;
 use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyByPath;
 use Stancl\Tenancy\Resolvers\PathTenantResolver;
 
-Route::middleware('tenant')->prefix('{'.PathTenantResolver::$tenantParameterName.'}')->group(function () {
-    Route::name('home.')->group(function () {
-        Route::get('/', SchoolHomeController::class)->name('index');
-        Route::get('/documents', [SchoolDocumentController::class, 'index'])->name('documents');
-        Route::get('/documents/download/{id}', [SchoolDocumentController::class, 'download'])->name('documents.download');
+Route::middleware(
+    [
+        'web',
+        InitializeTenancyByPath::class,
+        SetDefaultTenant::class]
+)
+    ->prefix('{'.PathTenantResolver::$tenantParameterName.'}')
+    ->group(function () {
+        Route::name('home.')->group(function () {
+            Route::get('/', SchoolHomeController::class)->name('index');
+            Route::get('/documents', [SchoolDocumentController::class, 'index'])->name('documents');
+            Route::get('/documents/download/{id}', [SchoolDocumentController::class, 'download'])->name('documents.download');
+        });
+
+        require __DIR__.'/admin.php';
+        require __DIR__.'/regiweb.php';
+        require __DIR__.'/parents.php';
+        require __DIR__.'/foro.php';
+
     });
-
-    require __DIR__.'/admin.php';
-    require __DIR__.'/regiweb.php';
-    require __DIR__.'/parents.php';
-    require __DIR__.'/foro.php';
-
-});
