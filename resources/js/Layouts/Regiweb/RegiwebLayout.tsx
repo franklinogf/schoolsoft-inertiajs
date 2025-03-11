63497db0173a394b0a46abd2e7531df9467bdb50
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-11 01:52:32 -0400

    Refactor Header component to use Ziggy routes and remove user prop

--- resources/js/Layouts/Regiweb/RegiwebLayout.tsx
+++ resources/js/Layouts/Regiweb/RegiwebLayout.tsx
@@ -1,7 +1,4 @@
 import Header from "@/Layouts/Regiweb/Header";
-import { PagePropsWithUser } from "@/types";
-import { Teacher } from "@/types/teacher";
-import { usePage } from "@inertiajs/react";
 import MainLayout from "../MainLayout";
 
 interface RegiwebLayoutProps {
@@ -9,13 +6,10 @@ interface RegiwebLayoutProps {
   title: string;
 }
 export function RegiwebLayout({ children, title }: RegiwebLayoutProps) {
-  const {
-    auth: { user },
-  } = usePage<PagePropsWithUser<Teacher>>().props;
   return (
     <MainLayout title={title}>
       <div className="flex min-h-dvh flex-col">
-        <Header user={user} />
+        <Header />
         <main className="px-2 pt-4 pb-8">{children}</main>
       </div>
     </MainLayout>
