faa213cdeb2414b6172efd8f0874a58ad02ef018
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-10 22:31:35 -0400

    Update HomePageTest to use test() function and assert school name

--- tests/Tenant/HomePageTest.php
+++ tests/Tenant/HomePageTest.php
@@ -1,7 +1,11 @@
 <?php
 
-it('renders school home page', function () {
+test('renders school home page', function () {
+    /** @var \Tests\TenantCase $this */
+    $schoolInfo = $this->getPrimaryAdmin();
+
     $response = $this->get(route('home.index'));
 
     $response->assertStatus(200);
+    $response->assertSee($schoolInfo->colegio);
 });
