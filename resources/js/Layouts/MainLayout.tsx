7417a375df3b23ccfb9b73cc75f70019c8249911
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-14 03:14:58 -0400

    Refactor theme handling in MainLayout and SchoolResource for improved CSS variable management

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import ConfirmationDialog from "@/Components/ConfirmationDialog";
 import { Toaster } from "@/Components/ui/sonner";
 import { useFlashToaster } from "@/hooks/flashToaster";
-import { PageProps } from "@/types";
+import { PageProps, themes } from "@/types";
 import { Head, usePage } from "@inertiajs/react";
 import { useEffect } from "react";
 interface MainLayoutProps {
@@ -11,87 +11,33 @@ interface MainLayoutProps {
 }
 export default function MainLayout({ children, title, description }: MainLayoutProps) {
   useFlashToaster();
-  const {
-    themes: { light, dark },
-    current,
-  } = usePage<PageProps>().props.theme;
+  const { themes, current } = usePage<PageProps>().props.theme;
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", current);
+    document.documentElement.classList.add(current);
   }, [current]);
 
+  function slugify(text: string) {
+    return "--" + text.replace(/([A-Z])/g, "-$1").toLowerCase() + ": ";
+  }
+  const th: keyof themes = "light";
   return (
     <>
       <Head>
-        {/* <html data-theme={current} /> */}
         <title>{title}</title>
         {description && <meta name="description" content={description} />}
         <style>
           {`
-            :root {
-                --background: ${light.background};
-                --foreground: ${light.foreground};
-
-                --muted: ${light.muted};
-                --muted-foreground: ${light.mutedForeground};
-
-                --popover: ${light.popover};
-                --popover-foreground: ${light.popoverForeground};
-
-                --card: ${light.card};
-                --card-foreground: ${light.cardForeground};
-
-                --primary: ${light.primary};
-                --primary-foreground: ${light.primaryForeground};
-
-                --secondary: ${light.secondary};
-                --secondary-foreground: ${light.secondaryForeground};
-
-                --accent: ${light.accent};
-                --accent-foreground: ${light.accentForeground};
-
-                --destructive: ${light.destructive};
-                --destructive-foreground: ${light.destructiveForeground};
-
-                --border: ${light.border};
-                --input: ${light.input};
-                --ring: ${light.ring};
-
-                --radius: ${light.radius};
-            }
-
-            [data-theme="dark"] {
-                --background: ${dark.background};
-                --foreground: ${dark.foreground};
-
-                --muted: ${dark.muted};
-                --muted-foreground: ${dark.mutedForeground};
-
-                --popover: ${dark.popover};
-                --popover-foreground: ${dark.popoverForeground};
-
-                --card: ${dark.card};
-                --card-foreground: ${dark.cardForeground};
-
-                --primary: ${dark.primary};
-                --primary-foreground: ${dark.primaryForeground};
-
-                --secondary: ${dark.secondary};
-                --secondary-foreground: ${dark.secondaryForeground};
-
-                --accent: ${dark.accent};
-                --accent-foreground: ${dark.accentForeground};
-
-                --destructive: ${dark.destructive};
-                --destructive-foreground: ${dark.destructiveForeground};
-
-                --border: ${dark.border};
-                --input: ${dark.input};
-                --ring: ${dark.ring};
-
-                --radius: ${dark.radius};
-            }
-            `}
+        ${Object.keys(themes)
+          .map((key) => {
+            const theme = themes[key as keyof themes];
+            const head = key === "light" ? "root:" : `.${key}`;
+            return `${head} {\n${Object.entries(theme)
+              .map(([k, v]) => `${slugify(k)} ${v};`)
+              .join("\n")}\n}`.trim();
+          })
+          .join("\n")}
+        `}
         </style>
       </Head>
       {children}
