ebacd6eb396f504c0e0d19eb3d38cfac4865a603
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-14 18:08:56 -0400

    Formatted files

--- resources/js/Layouts/Root/Footer.tsx
+++ resources/js/Layouts/Root/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <footer className="bg-secondary">
       <div className="mx-auto flex flex-col items-center px-5 py-8 sm:flex-row sm:justify-between">
         <Logo />
-        <p className="mt-4 text-sm text-secondary-foreground sm:ml-4 sm:mt-0 sm:py-2 sm:pl-4">
+        <p className="text-secondary-foreground mt-4 text-sm sm:mt-0 sm:ml-4 sm:py-2 sm:pl-4">
           © {currentYear}
         </p>
       </div>
