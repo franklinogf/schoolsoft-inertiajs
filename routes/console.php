59f8517f540d15e16f9a679ea4e7743cd2f9d177
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-15 16:23:47 -0400

    Add queue work command to run every minute with specified options

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,5 @@ use App\Console\Commands\DeleteTempUploadedFiles;
 Schedule::command('telescope:prune --hours=48')->description('Delete entries from telescope tables daily')->daily();
 
 Schedule::command(DeleteTempUploadedFiles::class)->hourly();
+
+Schedule::command('queue:work --stop-when-empty --tries=3 --timeout=90')->everyMinute();
