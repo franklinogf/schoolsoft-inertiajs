6a8980cb3312eb039aaea029b4a02cab4ecb1b08
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-15 21:37:32 -0400

    Add custom path and URL generators for media handling

---
+++ app/Services/MediaLibrary/CustomPathGenerator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services\MediaLibrary;
+
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class CustomPathGenerator implements PathGenerator
+{
+    /*
+     * Get the path for the given media, relative to the root storage path.
+     */
+    private function getBasePath(Media $media): string
+    {
+        $id = md5($media->id.config('app.key'));
+
+        return "{$media->collection_name}/{$id}/";
+    }
+
+    public function getPath(Media $media): string
+    {
+        return $this->getBasePath($media);
+    }
+
+    /*
+     * Get the path for conversions of the given media, relative to the root storage path.
+     */
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->getBasePath($media).'/conversions/';
+    }
+
+    /*
+     * Get the path for responsive images of the given media, relative to the root storage path.
+     */
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getBasePath($media).'/responsive-images/';
+    }
+}
