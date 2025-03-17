389a70bbe3c52c1e23c2158bbf7591424e85ecac
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-17 01:40:02 -0400

    Refactor OptionsMenu to use translations for titles and labels

--- resources/js/Components/OptionsMenu.tsx
+++ resources/js/Components/OptionsMenu.tsx
@@ -1,23 +1,25 @@
 import { Button } from "@/Components/ui/button";
+import { useTranslations, type Translations } from "@/hooks/translations";
 import { Link } from "@inertiajs/react";
-export type MenuOption = { title: string; items: { label: string; route: string }[] };
+export type MenuOption = { title: Translations; items: { label: Translations; route: string }[] };
 interface OptionsMenuProps {
   title: string;
   options: MenuOption[];
 }
 export function OptionsMenu({ title, options }: OptionsMenuProps) {
+  const { t } = useTranslations();
   return (
     <>
       <h1 className="page-primary-title">{title}</h1>
       <div className="mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {options.map((option, index) => (
+        {options.map(({ title, items }, index) => (
           <fieldset key={index} className="border-input border-2 p-2">
-            <legend className="text-lg font-semibold">{option.title}</legend>
+            <legend className="text-lg font-semibold">{t(title)}</legend>
             <ul className="space-y-4">
-              {option.items.map((item, index) => (
+              {items.map(({ route, label }, index) => (
                 <li key={index}>
                   <Button className="w-full" asChild>
-                    <Link href={item.route}>{item.label.toUpperCase()}</Link>
+                    <Link href={route}>{t(label).toUpperCase()}</Link>
                   </Button>
                 </li>
               ))}
