cbe6b35ddbaeafdfe48988269b3981c782d7ac96
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-23 19:00:47 -0400

    Update YesNo cast to return uppercase values; ensure consistent formatting for boolean representations

--- app/Casts/YesNo.php
+++ app/Casts/YesNo.php
@@ -14,7 +14,7 @@ class YesNo implements CastsAttributes
      */
     public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return ! $value ? 'NO' : $value;
+        return ! $value ? 'NO' : strtoupper($value);
     }
 
     /**
@@ -24,6 +24,6 @@ class YesNo implements CastsAttributes
      */
     public function set(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return ! $value ? 'NO' : $value;
+        return ! $value ? 'NO' : strtoupper($value);
     }
 }
