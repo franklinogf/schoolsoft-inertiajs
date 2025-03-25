b6b29f13a5587b9d13eef038d3d4ca7ec6afe7f2
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-25 18:42:50 -0400

    Implement CRUD operations for exam questions in QuestionTopicController

--- app/Http/Controllers/Regiweb/Options/Exam/QuestionTopicController.php
+++ app/Http/Controllers/Regiweb/Options/Exam/QuestionTopicController.php
@@ -2,10 +2,80 @@
 
 namespace App\Http\Controllers\Regiweb\Options\Exam;
 
+use App\Enums\FlashMessageKey;
+use App\Enums\YesNoEnum;
 use App\Http\Controllers\Controller;
+use App\Models\Exams\Exam;
+use App\Services\ExamService;
 use Illuminate\Http\Request;
 
 class QuestionTopicController extends Controller
 {
-    //
+    public function store(Request $request, Exam $exam)
+    {
+        $validated = $request->validate([
+            'pregunta' => ['required', 'string', 'max:255'],
+            'lineas' => ['required', 'numeric', 'min:1'],
+            'valor' => ['required', 'numeric', 'min:1'],
+        ]);
+
+        $exam->questions()->create($validated);
+        ExamService::updateExamTotal($exam);
+
+        return back()
+            ->with(
+                FlashMessageKey::SUCCESS->value,
+                __('Pregunta :action', ['action' => strtolower(__('Creada'))])
+            );
+    }
+
+    public function updateTitle(Request $request, Exam $exam)
+    {
+        $validated = $request->validate([
+            'titulo' => ['required', 'string', 'max:255'],
+        ]);
+
+        $exam->update([
+            'desc5' => YesNoEnum::YES->value,
+            'desc5_1' => $validated['titulo'],
+        ]);
+
+        return back()
+            ->with(
+                FlashMessageKey::SUCCESS->value,
+                __('Título :action', ['action' => strtolower(__('Actualizado'))])
+            );
+    }
+
+    public function update(Request $request, Exam $exam, $question)
+    {
+        $validated = $request->validate([
+            'pregunta' => ['required', 'string', 'max:255'],
+            'lineas' => ['required', 'numeric', 'min:1'],
+            'valor' => ['required', 'numeric', 'min:1'],
+        ]);
+
+        $exam->questions()->findOrFail($question)->update($validated);
+
+        ExamService::updateExamTotal($exam);
+
+        return back()
+            ->with(
+                FlashMessageKey::SUCCESS->value,
+                __('Pregunta :action', ['action' => strtolower(__('Actualizada'))])
+            );
+    }
+
+    public function destroy(Exam $exam, $question)
+    {
+        $exam->questions()->findOrFail($question)->delete();
+
+        ExamService::updateExamTotal($exam);
+
+        return back()
+            ->with(
+                FlashMessageKey::SUCCESS->value,
+                __('Pregunta :action', ['action' => strtolower(__('Eliminada'))])
+            );
+    }
 }
