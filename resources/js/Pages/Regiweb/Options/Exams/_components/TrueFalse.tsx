7cd56211ccc2f594161dc7b89b3c6071076665a8
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-25 12:06:03 -0400

    Enhance TrueFalse component to support examId prop and implement modal for adding/editing questions

--- resources/js/Pages/Regiweb/Options/Exams/_components/TrueFalse.tsx
+++ resources/js/Pages/Regiweb/Options/Exams/_components/TrueFalse.tsx
@@ -1,29 +1,135 @@
+import { FieldsGrid } from "@/Components/forms/inputs/FieldsGrid";
+import { InputField } from "@/Components/forms/inputs/InputField";
+import { SelectField } from "@/Components/forms/inputs/SelectField";
+import SubmitButton from "@/Components/forms/SubmitButton";
+import { badgeVariants } from "@/Components/ui/badge";
 import { Button } from "@/Components/ui/button";
-import { Topics } from "@/types/exam";
-import { PlusCircleIcon } from "lucide-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/Components/ui/dialog";
+import { Topics, TrueFalseTopic } from "@/types/exam";
+import { router, useForm } from "@inertiajs/react";
+import { EditIcon, PlusCircleIcon } from "lucide-react";
 import { Topic, TopicItem } from "./Topic";
 
-export function TrueFalse({ topic }: { topic: Topics["verdadero_falso"] }) {
+export function TrueFalse({ topic, examId }: { topic: Topics["verdadero_falso"]; examId: number }) {
   return (
-    <Topic amount={topic.preguntas.length} addButton={<AddButton />} title={topic.titulo}>
+    <Topic
+      amount={topic.preguntas.length}
+      addButton={<FormModal examId={examId} />}
+      title={topic.titulo}
+    >
       {topic.preguntas.map((question) => (
         <TopicItem
           key={question.id}
           label={question.pregunta}
           answer={question.respuesta === "v" ? "Verdadero" : "Falso"}
           value={question.valor}
-          onEdit={() => {}}
-          onDelete={() => {}}
+          editButton={<FormModal examId={examId} item={question} />}
+          onDelete={() => {
+            router.delete(
+              route("regiweb.options.exams.truefalse.destroy", {
+                exam: examId,
+                question: question.id,
+              }),
+            );
+          }}
         />
       ))}
     </Topic>
   );
 }
 
-function AddButton() {
+function FormModal({ examId, item }: { examId: number; item?: TrueFalseTopic }) {
+  const { data, setData, processing, post, errors, reset, put, clearErrors } = useForm(
+    `truefalse${item?.id || ""}`,
+    {
+      pregunta: item?.pregunta || "",
+      respuesta: item?.respuesta || "v",
+      valor: item?.valor || "",
+    },
+  );
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (item) {
+      put(route("regiweb.options.exams.truefalse.update", { exam: examId, question: item.id }));
+    } else {
+      post(route("regiweb.options.exams.truefalse.store", { exam: examId }), {
+        onSuccess: () => {
+          reset();
+        },
+      });
+    }
+  }
   return (
-    <Button className="size-6" size="icon">
-      <PlusCircleIcon />
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        {item ? (
+          <button className={badgeVariants({ variant: "secondary", className: "h-5" })}>
+            <EditIcon />
+          </button>
+        ) : (
+          <Button className="size-6" size="icon">
+            <PlusCircleIcon />
+          </Button>
+        )}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{item ? "Editar pregunta" : "Nueva pregunta"}</DialogTitle>
+          <DialogDescription hidden></DialogDescription>
+        </DialogHeader>
+        <form className="space-y-2" onSubmit={handleSubmit}>
+          <InputField
+            label="Pregunta"
+            value={data.pregunta}
+            onChange={(value) => setData("pregunta", value)}
+            error={errors.pregunta}
+            required
+          />
+          <FieldsGrid>
+            <SelectField
+              label="Respuesta"
+              value={data.respuesta}
+              onChange={(value) => setData("respuesta", value as TrueFalseTopic["respuesta"])}
+              items={[
+                { label: "Verdadero", value: "v" },
+                { label: "Falso", value: "f" },
+              ]}
+              error={errors.respuesta}
+              required
+            />
+            <InputField
+              label="Valor"
+              type="number"
+              value={data.valor}
+              onChange={(value) => setData("valor", value)}
+              error={errors.valor}
+              required
+            />
+          </FieldsGrid>
+          <div className="mt-4 flex justify-end gap-2">
+            <DialogClose asChild>
+              <Button
+                onClick={() => {
+                  reset();
+                  clearErrors();
+                }}
+                variant="outline"
+              >
+                Cancelar
+              </Button>
+            </DialogClose>
+            <SubmitButton disabled={processing}>{item ? "Editar" : "Agregar"}</SubmitButton>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
