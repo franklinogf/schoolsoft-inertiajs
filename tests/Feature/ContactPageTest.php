79b3efc7251d83b8f282ea355ab5272841d4ea6f
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-28 15:41:50 -0400

    Add feature tests for Contact page functionality

---
+++ tests/Feature/ContactPageTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Mail\Root\Contact;
+
+test('Can access Contact page', function () {
+    $response = $this->get(route('contact.index'));
+
+    $response->assertStatus(200);
+});
+
+test('Contact page can submit a form', function () {
+    Mail::fake();
+    $emailData = [
+        'name' => 'Franklin',
+        'lastname' => 'Flores',
+        'email' => 'test@test.com',
+        'message' => 'Hello, this is a test message',
+        'phone' => '9293394306',
+    ];
+    $response = $this->post(route('contact.submit'), $emailData);
+
+    Mail::assertSent(fn (Contact $mail): bool => $mail->name === $emailData['name']);
+
+    $response->assertRedirect(route('contact.index'));
+});
+
+test('Contact page cannot submit a form with invalid email', function () {
+    $response = $this->post(route('contact.submit'), [
+        'name' => 'Franklin',
+        'lastname' => 'Flores',
+        'email' => 'test',
+        'message' => 'Hello, this is a test message',
+        'phone' => '9293394306',
+    ]);
+    $response->assertSessionHasErrors('email');
+});
+
+test('Contact page cannot submit a form with invalid phone', function () {
+    $response = $this->post(route('contact.submit'), [
+        'name' => 'Franklin',
+        'lastname' => 'Flores',
+        'email' => 'test@test.com',
+        'message' => 'Hello, this is a test message',
+        'phone' => '12345678',
+    ]);
+    $response->assertSessionHasErrors('phone');
+});
