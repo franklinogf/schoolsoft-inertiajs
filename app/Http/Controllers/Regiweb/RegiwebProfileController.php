e1f200773f8cf2e7820afc01afe31662c0f1ea26
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-30 17:54:24 -0400

    Refactor RegiwebProfileController to utilize TeacherService for profile updates and picture management

--- app/Http/Controllers/Regiweb/RegiwebProfileController.php
+++ app/Http/Controllers/Regiweb/RegiwebProfileController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\Regiweb;
 
-use App\Enums\MediaCollectionEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Regiweb\ProfileUpdateRequest;
-use App\Models\TemporaryFile;
+use App\Services\TeacherService;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
@@ -23,33 +22,20 @@ class RegiwebProfileController extends Controller
     /**
      * Update the resource in storage.
      */
-    public function update(ProfileUpdateRequest $request)
+    public function update(ProfileUpdateRequest $request, TeacherService $teacherService)
     {
         $validated = $request->validated();
         $data = $request->safe()->except('picture');
-
-        /**
-         * @var \App\Models\Teacher $user
-         */
-        $user = $request->user();
-        $user->fill($data);
-
         $folder = $validated['picture'];
+
+        $user = $request->user();
+
+        $teacherService->update($user, $data);
 
         if ($folder !== null) {
 
-            $temporaryFile = tenancy()->central(function () use ($folder): TemporaryFile|null {
-                return TemporaryFile::where('folder', $folder)->first();
-            });
-
-            if ($temporaryFile) {
-                $user->addMediaFromDisk(tmp_path($folder, $temporaryFile->filename), 'local')
-                    ->toMediaCollection(MediaCollectionEnum::PROFILE_PICTURE->value);
-                $temporaryFile->delete();
-            }
+            $teacherService->addProfilePicture($user, $folder);
         }
-
-        $user->save();
 
         return back()->with('success', __('Perfil actualizado con éxito'));
 
