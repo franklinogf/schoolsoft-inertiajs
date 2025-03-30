e34c62831c5d3da6c5297bfcc0d2593b6a882489
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-03-29 23:15:24 -0400

    Add support for multiple central domains in tenancy configuration and update routes accordingly

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,20 @@ use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', fn () => Inertia::render('Root/Index'))->name('home');
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('/', fn () => Inertia::render('Root/Index'))->name('home');
 
-Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
-Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
+        Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+        Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
-Route::get('/modules', fn () => Inertia::render('Root/Modules'))->name('modules');
+        Route::get('/modules', fn () => Inertia::render('Root/Modules'))->name('modules');
 
-Route::get('/regiweb', fn () => Inertia::render('Root/Regiweb'))->name('regiweb');
+        Route::get('/regiweb', fn () => Inertia::render('Root/Regiweb'))->name('regiweb');
 
-Route::controller(UploadController::class)->group(function () {
-    Route::post('upload', 'store');
-    Route::delete('upload', 'destroy');
-});
+        Route::controller(UploadController::class)->group(function () {
+            Route::post('upload', 'store');
+            Route::delete('upload', 'destroy');
+        });
+    });
+}
